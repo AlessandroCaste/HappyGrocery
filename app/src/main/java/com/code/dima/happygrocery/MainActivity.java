877a541b778ed72497b98f966e04fdcdfed7168d
AlessandroCaste <castellani.alessandro93@gmail.com>
2018-11-21 19:29:54 +0100

    Work super in progress

--- app/src/main/java/com/code/dima/happygrocery/MainActivity.java
+++ app/src/main/java/com/code/dima/happygrocery/MainActivity.java
@@ -2,8 +2,11 @@ package com.code.dima.happygrocery;
 
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +20,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.alessandro.barcodeyeah.R;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -31,6 +38,9 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView recyclerView;
     private ProductAdapter adapter;
     private ArrayList<Product> productArrayList;
+    ViewPager viewPager;
+    SlideAdapter myPagerAdapter;
+    Integer x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         Log.w("myApp", "Avvio");
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+     /*   DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -59,7 +69,11 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setAdapter(adapter);
 
 
-        createDummyData();
+        createDummyData(); */
+        viewPager = (ViewPager)findViewById(R.id.myviewpager);
+        myPagerAdapter = new SlideAdapter();
+        viewPager.setAdapter(myPagerAdapter);
+
     }
 
 
@@ -162,6 +176,70 @@ public class MainActivity extends AppCompatActivity
             super.onActivityResult(requestCode,resultCode,data);
         }
     }
+
+    private class SlideAdapter extends PagerAdapter {
+
+    int NumberOfPages = 5;
+
+    int[] res = {
+            android.R.drawable.ic_dialog_alert,
+            5,
+            android.R.drawable.ic_menu_compass,
+            android.R.drawable.ic_menu_directions,
+            android.R.drawable.ic_menu_gallery};
+    int[] backgroundcolor = {
+            0xFF101010,
+            0xFF202020,
+            0xFF303030,
+            0xFF404040,
+            0xFF505050};
+
+    @Override
+    public int getCount() {
+        return NumberOfPages;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        TextView textView = new TextView(MainActivity.this);
+        RecyclerView recyclerView= new RecyclerView(MainActivity.this);
+        textView.setTextSize(30);
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        textView.setText(String.valueOf(position));
+
+        ImageView imageView = new ImageView(MainActivity.this);
+        imageView.setImageResource(res[position]);
+        ViewGroup.LayoutParams imageParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        imageView.setLayoutParams(imageParams);
+
+        LinearLayout layout = new LinearLayout(MainActivity.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        layout.setBackgroundColor(backgroundcolor[position]);
+        layout.setLayoutParams(layoutParams);
+        layout.addView(textView);
+        layout.addView(imageView);
+
+        final int page = position;
+
+        //container.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object)     {
+        container.removeView((LinearLayout)object);
+    }
+
+}
 
 
 
