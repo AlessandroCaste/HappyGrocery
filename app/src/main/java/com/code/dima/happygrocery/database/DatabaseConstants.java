efa1a60b3d5faf98623f45f63c0ed5fe4096e723
FabioCastle5 <fabio1.castello@mail.polimi.it>
2018-11-26 23:40:57 +0100

    Added DatabaseHelper class and defined some constants

---
+++ app/src/main/java/com/code/dima/happygrocery/database/DatabaseConstants.java
@@ -0,0 +1,63 @@
+package com.code.dima.happygrocery.database;
+
+public class DatabaseConstants {
+
+
+    // this class is not going to be instantiated
+    private DatabaseConstants () {
+        return;
+    }
+
+
+    // global constants
+    public static final String DB_NAME = "happy_grocery.db";
+    public static final int DB_VERSION = 1;
+
+
+    // Grocery history column names
+    public static final String HISTORY_TABLE = "grocery_history";
+    public static final String HISTORY_ID = "id";
+    public static final String HISTORY_DATE = "date";
+    public static final String HISTORY_MARKET = "market";
+    public static final String HISTORY_AMOUNT = "amount";
+    public static final String HISTORY_ACTIVE = "active";
+
+    // Product column names
+    public static final String PRODUCT_TABLE = "product";
+    public static final String PRODUCT_ID = "id";
+    public static final String PRODUCT_NAME = "name";
+    public static final String PRODUCT_WEIGHT = "weight";
+    public static final String PRODUCT_CATEGORY = "category";
+
+    // Product list column names
+    public static final String LIST_TABLE = "product_list";
+    public static final String LIST_HID = "h_id";
+    public static final String LIST_PID = "p_id";
+    public static final String LIST_QUANTITY = "quantity";
+    public static final String LIST_PRICE = "price";
+
+
+    // create queries used to instantiate tables in the database
+    public static final String CREATE_GROCERY_HISTORY_TABLE =
+            "CREATE TABLE " + HISTORY_TABLE + "("
+            + HISTORY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + HISTORY_DATE + " TEXT, "
+            + HISTORY_MARKET + " TEXT, "
+            + HISTORY_AMOUNT + " REAL, "
+            + HISTORY_ACTIVE + " INTEGER)";
+
+    public static final String CREATE_PRODUCT_TABLE =
+            "CREATE TABLE " + PRODUCT_TABLE + "("
+                    + PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + PRODUCT_NAME + " TEXT, "
+                    + PRODUCT_WEIGHT + " REAL, "
+                    + PRODUCT_CATEGORY + " TEXT)";
+
+    public static final String CREATE_PRODUCT_LIST_TABLE =
+            "CREATE TABLE " + LIST_TABLE + "("
+                + LIST_HID + " INTEGER REFERENCES grocery_history(id) NOT NULL, "
+                + LIST_PID + " INTEGER REFERENCES product(id) NOT NULL, "
+                + LIST_QUANTITY + " INTEGER, "
+                + LIST_PRICE + " REAL, "
+                + "PRIMARY KEY (h_id, p_id))";
+}
---
+++ app/src/main/java/com/code/dima/happygrocery/database/DatabaseHelper.java
@@ -0,0 +1,50 @@
+package com.code.dima.happygrocery.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static DatabaseHelper instance = null;
+    private Context context;
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DatabaseConstants.DB_NAME, null, DatabaseConstants.DB_VERSION);
+        this.context = context;
+    }
+
+    public static DatabaseHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseConstants.CREATE_GROCERY_HISTORY_TABLE);
+        db.execSQL(DatabaseConstants.CREATE_PRODUCT_TABLE);
+        db.execSQL(DatabaseConstants.CREATE_PRODUCT_LIST_TABLE);
+    }
+
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + DatabaseConstants.HISTORY_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DatabaseConstants.PRODUCT_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DatabaseConstants.LIST_TABLE);
+            onCreate(db);
+        }
+    }
+}
