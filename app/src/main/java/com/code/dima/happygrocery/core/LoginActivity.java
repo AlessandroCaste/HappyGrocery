22021c1abf69ec0370ee21875004236a99d0132e
AlessandroCaste <32411739+AlessandroCaste@users.noreply.github.com>
2018-12-13 14:59:04 +0100

    Rimozione layout inutili

--- app/src/main/java/com/code/dima/happygrocery/core/LoginActivity.java
+++ app/src/main/java/com/code/dima/happygrocery/core/LoginActivity.java
@@ -69,7 +69,7 @@ public class LoginActivity extends BaseActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-                setContentView(R.layout.activity_google);
+                setContentView(R.layout.login_activity);
                 context = this;
 
                 // Initialize Facebook Login button
