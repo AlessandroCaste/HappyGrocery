327d22c8b698d10490988e857593fe3f2fd6cf8d
FabioCastle5 <fabio1.castello@mail.polimi.it>
2019-06-06 19:13:24 +0200

    Cleared unused stuff in wear module

--- wear/src/main/java/com/code/dima/happygrocery/utils/DataPaths.java
+++ wear/src/main/java/com/code/dima/happygrocery/utils/DataPaths.java
@@ -10,7 +10,6 @@ public class DataPaths {
     public static final String QUANTITIES_KEY = "com.code.dima.happygrocery.quantities";
 
     // message keys
-    public static final String NOTIFY_CONNECTED = "/notify_connection_from_wearable";
     public static final String NOTIFY_NEW_GROCERY = "/notify_new_grocery_from_phone";
     public static final String NOTIFY_NEW_PRODUCT = "/notify_new_product_from_phone";
     public static final String NOTIFY_GROCERY_CLOSED = "/notify_grocery_closed_from_phone";
@@ -19,11 +18,4 @@ public class DataPaths {
     // capabilities
     public static final String WATCH_SERVER = "watch_server";
     public static final String WATCH_CLIENT = "watch_client";
-
-    // action for broadcast intents sent by communication service
-    public static final String ACTION_NOTIFICATION = "notification";
-    public static final String ACTION_CONNECTED = "connected";
-    public static final String ACTION_DISCONNECTED = "disconnected";
-    public static final String ACTION_UPDATE_AMOUNT = "update_amount";
-    public static final String ACTION_UPDATE_QUANTITIES = "update_quantities";
 }
