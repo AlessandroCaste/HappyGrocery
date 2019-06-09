998ad7a1a174bdd3280c75f572fa90b0bbbcc736
FabioCastle5 <fabio1.castello@mail.polimi.it>
2019-06-09 12:39:41 +0200

    Updated view and behaviour of the product details activity in wear module

--- wear/src/main/java/com/code/dima/happygrocery/activity/ProductDetailsActivity.java
+++ wear/src/main/java/com/code/dima/happygrocery/activity/ProductDetailsActivity.java
@@ -3,14 +3,19 @@ package com.code.dima.happygrocery.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.wear.widget.CircularProgressLayout;
 
 import com.code.dima.happygrocery.R;
 import com.code.dima.happygrocery.utils.Category;
 import com.code.dima.happygrocery.utils.ImageRetriever;
 
 public class ProductDetailsActivity extends WearableActivity {
+
+    private CircularProgressLayout progressLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +30,33 @@ public class ProductDetailsActivity extends WearableActivity {
         Intent intent = getIntent();
         String nameString = intent.getStringExtra("name");
         name.setText(nameString);
-        quantity.setText(intent.getStringExtra("quantity"));
-        price.setText(intent.getStringExtra("price"));
+        String quantityString = "quantity: " + intent.getStringExtra("quantity");
+        quantity.setText(quantityString);
+        String priceString = "price: " + intent.getStringExtra("price");
+        priceString += getString(R.string.currency);
+        price.setText(priceString);
         Category category = Category.valueOf(intent.getStringExtra("category"));
 
         ImageRetriever retriever = ImageRetriever.getInstance(this);
         int imageID = retriever.retrieveImageID(nameString, category);
         image.setImageResource(imageID);
 
-        // Enables Always-on
+        progressLayout = findViewById(R.id.product_details_progress_layout);
+        progressLayout.setTotalTime(5000);
+
+        progressLayout.setOnTimerFinishedListener(new CircularProgressLayout.OnTimerFinishedListener() {
+            @Override
+            public void onTimerFinished(CircularProgressLayout layout) {
+                // returns to Dashboard
+                finish();
+            }
+        });
+
         setAmbientEnabled();
+    }
+
+    public void onClick(View view) {
+        progressLayout.stopTimer();
+        finish();
     }
 }
